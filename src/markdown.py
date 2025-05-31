ce9c38b654a9dff204d558c9c1845fdfe7f63cba
Taviquenson <tavo2070@gmail.com>
2025-05-31 11:38:58 -0700

    extract links from Markdown

--- src/markdown.py
+++ src/markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -18,7 +19,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 # enclosed by delimiters, as in **bold**, split() will return the list
                 # [ "", bold, ""]
                 if len(words) % 2 == 0: #list is even, there was no closing delimiter
-                    raise ValueError("Invalid Markdown syntax")
+                    raise ValueError("Invalid Markdown, formatted section not closed")
                 # Edge case of the delimited word being the first word or last word,
                 # meaning split() created an empty string at the beginning or the end
                 if words[i] == "":
@@ -32,3 +33,25 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     new_nodes.append(TextNode(words[i], text_type))
 
     return new_nodes
+
+def extract_markdown_images(text):
+    # returns matches for 2 groups along the string in a list of tuples
+    # matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text) # first regex idea
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text) # more robust regex
+    return matches # list of tuples [(alt_text, link), ...]
+
+def extract_markdown_links(text):
+    # returns matches for 2 groups along the string in a list of tuples
+    # matches = re.findall(r"\[(.*?)\]\((.*?)\)", text) # first regex idea
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text) # more robust regex
+    return matches # list of tuples [(link_text, link), ...]
+
+# Regexes ([^\[\]]*) and ([^\(\)]*) are about including characters other than '[' and ']' in
+# the first group and other than '(' and ')' in the second group
+# e.g. anything inside brackets of [^] means "match any character that is not in the set"
+#      Thus, in [^\[\]] both characters '[' and ']' won't get matched
+
+# In regex (?<!!), means "negtive look behind applied to character '!' "
+# Negative look behind specifies a group that can not match before the main expression
+# (if it matches, the result is discarded)
+# Basically, if there is a '!' before the rest of the matched pattern, the result is discarded
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,8 +1,9 @@
 import unittest
-from markdown import split_nodes_delimiter
+from markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 class TestMarkdown(unittest.TestCase):
+    ''' split_nodes_delimiter() - tests'''
     def test_code_in_text(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
@@ -67,3 +68,62 @@ class TestMarkdown(unittest.TestCase):
             TextNode("code block", TextType.CODE),
         ]
         self.assertEqual(new_nodes, expected)
+
+
+    ''' extract_markdown_images() - regex tests'''
+    def test_extract_md_images_1img(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_md_images_2imgs(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+
+        )
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"),("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+    
+    def test_extract_md_images_3imgs(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif), ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) and ![image](https://i.imgur.com/zjjcJKZ.png)"
+
+        )
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"),("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"), ("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_md_images_empty_alt_text(self):
+        matches = extract_markdown_images(
+            "This is text with an ![](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_md_images_empty_url(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image]()"
+        )
+        self.assertListEqual([("image", "")], matches)
+
+    ''' extract_markdown_links() - regex tests'''
+    def test_extract_md_links_1link(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
+
+    def test_extract_md_links_2links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+
+    def test_extract_md_links_3links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev), [to youtube](https://www.youtube.com/@bootdotdev) and to [google](https://www.google.com)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev"), ("google", "https://www.google.com")], matches)
+    
+    def test_extract_md_links_with_img(self):
+        matches = extract_markdown_links(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([], matches)
