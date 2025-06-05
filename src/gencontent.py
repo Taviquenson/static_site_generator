ac390999fb49c3432c22c53e82072b4d09dfa27e
Taviquenson <tavo2070@gmail.com>
2025-06-05 10:03:59 -0700

    generate pages v2

--- src/gencontent.py
+++ src/gencontent.py
@@ -12,11 +12,11 @@ def extract_title(markdown):
 
 ''' takes a Markdown file and generates an html of it '''
 def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    # print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     md_file = open(from_path, "r")
     md_str = md_file.read()
-    # print(md_str)
+
     html_template_file = open(template_path, "r")
     html_template_str = html_template_file.read()
 
@@ -26,7 +26,6 @@ def generate_page(from_path, template_path, dest_path):
 
     # replace placeholders in template html
     html_final_str = html_template_str.replace("{{ Title }}", title_str).replace("{{ Content }}", page_html_str)
-    # print(html_final_str)
 
     # get path before file name
     directory_path = os.path.dirname(dest_path)
@@ -35,7 +34,6 @@ def generate_page(from_path, template_path, dest_path):
         print(f"Created directory '{directory_path}'")
 
     if os.path.exists(directory_path):
-        # html_final_path = os.path.join(dest_path, "index.html")
         with open(dest_path, 'w') as file: # file is closed after 'with' block
             file.write(html_final_str)
     else:
@@ -57,7 +55,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     
     for file_name in file_names:
         new_dir_path_content = os.path.join(dir_path_content, file_name)
-        if os.path.isfile(new_dir_path_content):
+        if os.path.isfile(new_dir_path_content) and file_name.endswith(".md"):
             print(f"Creating page '{new_dir_path_content}' in dir: {dest_dir_path}")
             generate_page(
                 new_dir_path_content,
@@ -66,5 +64,5 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             )
         else: # was directory, so recurse
             next_dest_dir_path = os.path.join(dest_dir_path, file_name)
-            # print(f"Recursing into dir: {new_dir_path_content} and Copying to dir: {next_dest_dir}")
+
             generate_pages_recursive(new_dir_path_content, template_path, next_dest_dir_path)
